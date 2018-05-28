d4c563e4f30aed86867d466e8c88851e697345ce
lancelottran <39706009+lancelottran@users.noreply.github.com>
2018-05-28 09:18:44 -0700

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,3 +1,12 @@
+/**
+Because 1 ≤ a.length ≤ 105 and 1 ≤ a[i] ≤ a.length
+so a can not have a negative number
+We will check value at a[i-1] if it is > 0
+mean it is our first visit and we will make it negative
+If a[i - 1] < 0 mean it already visited and we should
+return that value
+**/
+
 function firstDuplicate(a) {
     for(var i = 0; i < a.length; i++){
         var temp = a[i];
